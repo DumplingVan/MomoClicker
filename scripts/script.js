26a95303cf08c964aafc28200c593c9442750788
DumplingVan <suvan691@gmail.com>
2026-05-11 14:44:39 -0400

    Added lots of functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,22 @@
 const Momo = document.querySelector(".Dumplings");
+const counter = document.querySelector(".count");
+
+let clicks = 0;
 
 Momo.addEventListener("click", () => {
-    console.log("clicked");
+    clicks += 1;
+    console.log(`Clicked, counter: ${clicks}`);
+    counter.textContent = `Dumplings: ${clicks}`;
 
-    Momo.style.scale = "8%";
+    Momo.style.transition = "0.05s";
+    Momo.classList.add("clicked")
+
+    setTimeout(() => {
+        Momo.classList.remove("clicked")
+    }, 50);
     
+    Momo.style.transition = "0.3s";
 });
 
+counter.textContent = `Dumplings: ${clicks}`;
 console.log("script successfully loaded.");
