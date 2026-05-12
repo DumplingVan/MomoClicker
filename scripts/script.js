29ca038355c2fefac4ede068517e2b7c17ecee24
DumplingVan <suvan691@gmail.com>
2026-05-12 10:46:36 -0400

    Added modern code/simplicity
    updated from many functions to classes in scripts.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,60 @@ const counter = document.querySelector(".count");
 const persec = document.querySelector(".persec");
 const cursorPurchase = document.getElementById("Cursor");
 const steamerPurchase = document.getElementById("Steamer");
+const factoryPurchase = document.getElementById("Dumpling-factory");
+const friedPurchase = document.getElementById("Fried-Dumplings");
 const displayCursor = document.getElementById("cursorprice");
 const displaySteamer = document.getElementById("steamerprice");
+const displayFactory = document.getElementById("factoryprice");
+const displayFried = document.getElementById("friedprice");
 
 let clicks = 0;
-let shopProg = 0;
-let cursors = 0;
-let cursorPrice = 5;
-let steamers = 0;
-let steamerPrice = 50;
-let factories = 0;
-let factoryPrice = 500;
+let shopProg = 1;
+
+class Item {
+    static allItems = [];
+
+    constructor(element, priceDisplay, Name, Price, amount, Prog) {
+        this.element = element;
+        this.priceDisplay = priceDisplay;
+        this.Name = Name;
+        this.Price = Price;
+        this.amount = amount;
+        this.Prog = Prog;
+
+        Item.allItems.push(this);
+        this.priceDisplay.textContent = `$${this.Price}`
+        element.addEventListener("click", this.Purchase);
+    }
+    Purchase = event => {
+        if (clicks >= this.Price) {
+            this.amount += 1;
+            clicks -= this.Price;
+            this.Price = Math.floor(this.Price * 1.5);
+            this.priceDisplay.textContent = `$${this.Price}`
+            
+            CountUpdate()
+        }
+        else{
+            //placeholder
+        };
+
+        if (this.Prog == shopProg) {
+            shopProg += 1;
+            Item.allItems.forEach(items => items.shopUpdate());
+        }
+    }
+    shopUpdate() {
+        if (shopProg == this.Prog) {
+            this.element.classList.add("show");
+        }
+    };
+}
 
 Momo.addEventListener("click", () => {
     clicks += 1;
     console.log(`Clicked, counter: ${clicks}`);
-    PriceUpdate()
+    CountUpdate()
     
     Momo.style.transition = "0.05s";
     Momo.classList.add("clicked")
@@ -30,76 +68,27 @@ Momo.addEventListener("click", () => {
     Momo.style.transition = "0.3s";
 });
 
-cursorPurchase.addEventListener("click", () => {
-    if (clicks >= cursorPrice) {
-        cursors += 1;
-        clicks -= cursorPrice;
-        cursorPrice = Math.floor(cursorPrice * 1.5);
-
-        PriceUpdate()
-    }
-    else{
-        //placeholder
-    };
-
-    if (shopProg == 1) {
-        shopUpdate();
-    };
-});
-
-steamerPurchase.addEventListener("click", () => {
-    if (clicks >= steamerPrice) {
-        steamers += 1;
-        clicks -= steamerPrice;
-        steamerPrice = Math.floor(steamerPrice * 1.5);
-
-        PriceUpdate()
-    }
-    else{
-        //placeholder
-    };
-    
-    if (shopProg == 2) {
-        shopUpdate();
-    };
-});
-
 function addDumplings() {
-    let add = cursors + steamers * 10 + factories * 50;
+    let add = Cursors.amount + Steamers.amount * 10 + Factories.amount * 50;
     console.log(add);
 
     clicks += add;
-    PriceUpdate()
+    CountUpdate()
 };
 
-function PriceUpdate() {
+function CountUpdate (){
     counter.textContent = `Dumplings: ${clicks}`;
-    persec.textContent = `${cursors + steamers * 10 + factories * 50} Dumplings / per sec`;
+    persec.textContent = `${Cursors.amount + Steamers.amount * 10 + Factories.amount * 50} Dumplings / per sec`;
+}
 
-    displayCursor.textContent = "$" + String(cursorPrice);
-    displaySteamer.textContent = "$" + String(steamerPrice);
-};
-
-function shopUpdate() {
-    shopProg += 1;
-
-    if (shopProg == 1) {
-        cursorPurchase.classList.add("show")
-    }
-    else if (shopProg == 2) {
-        steamerPurchase.classList.add("show")
-    }
-    else if (shopProg == 3) {
-
-    }
-    else if (shopProg == 4) {
-        
-    }
-};
+console.log("script successfully loaded.");
+let Cursors = new Item(cursorPurchase, displayCursor, "Cursor", 5, 0, 1);
+let Steamers = new Item(steamerPurchase, displaySteamer, "Steamer", 50, 0, 2);
+let Factories = new Item(factoryPurchase, displayFactory, "Factory", 500, 0, 3);
+let FriedMomos = new Item(friedPurchase, displayFried, "Fried Momos", 10000, 0, 4);
 
 counter.textContent = `Dumplings: ${clicks}`;
-console.log("script successfully loaded.");
+cursorPurchase.classList.add("show")
 
+CountUpdate();
 setInt = setInterval(addDumplings, 1000);
-PriceUpdate();
-shopUpdate();
